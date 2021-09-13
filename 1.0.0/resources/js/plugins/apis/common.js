e1a9de1232a6c945f7499a54961812b74a147535
wzj <wzj@ddbff460-bf18-274e-8a17-2e708027881f>
2021-09-13 01:13:33 +0000

    git-svn-id: https://39.108.144.9:8443/svn/afanti_shop@21 ddbff460-bf18-274e-8a17-2e708027881f

--- 1.0.0/resources/js/plugins/apis/common.js
+++ 1.0.0/resources/js/plugins/apis/common.js
@@ -1,5 +1,5 @@
-//const baseUrl = 'http://afanti_shop.g.com/api/'; // 接口公共域名
-const baseUrl = 'http://afanti_shop.feibu.info/api/'; // 接口公共域名
+const baseUrl = 'http://afanti_shop.g.com/api/'; // 接口公共域名
+//const baseUrl = 'http://afanti_shop.feibu.info/api/'; // 接口公共域名
 export default
 {
     baseUrl,

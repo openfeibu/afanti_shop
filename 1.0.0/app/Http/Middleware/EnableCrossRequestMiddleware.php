f937d42dd6674ca6d41f600f04e1bc43cf383ad8
wzj <wzj@ddbff460-bf18-274e-8a17-2e708027881f>
2021-09-13 01:01:53 +0000

    git-svn-id: https://39.108.144.9:8443/svn/afanti_shop@18 ddbff460-bf18-274e-8a17-2e708027881f

--- 1.0.0/app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ 1.0.0/app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -16,12 +16,7 @@ class EnableCrossRequestMiddleware
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Credentials: true");
-        header("Access-Control-Allow-Methods: *");
-        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
-        header("Access-Control-Expose-Headers: *");
-        /*
+
         $response = $next($request);
         if(is_array($response)){
 	        $response = response()->json($response);
@@ -34,6 +29,5 @@ class EnableCrossRequestMiddleware
 			$response->header('Access-Control-Allow-Credentials', 'true');
 		}
         return $response;
-        */
     }
 }

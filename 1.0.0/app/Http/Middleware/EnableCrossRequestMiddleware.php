4f348190aa1fab3bf44b5c25495e66438b12ee07
wzj <wzj@ddbff460-bf18-274e-8a17-2e708027881f>
2021-09-13 01:11:45 +0000

    git-svn-id: https://39.108.144.9:8443/svn/afanti_shop@19 ddbff460-bf18-274e-8a17-2e708027881f

--- 1.0.0/app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ 1.0.0/app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Log;
 use Closure;
+use Request;
 
 class EnableCrossRequestMiddleware
 {
@@ -16,18 +17,18 @@ class EnableCrossRequestMiddleware
      */
     public function handle($request, Closure $next)
     {
-
+		
         $response = $next($request);
         if(is_array($response)){
 	        $response = response()->json($response);
         }
 		$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
-		if(in_array($origin, config('app.allow_origin'))){
+		//if(in_array($origin, config('app.allow_origin'))){
 			$response->header('Access-Control-Allow-Origin',  $origin);
 			$response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
 			$response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
 			$response->header('Access-Control-Allow-Credentials', 'true');
-		}
+		//}
         return $response;
     }
 }

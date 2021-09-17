c00810f7dc5f6487fdd72d2a39809313726172a2
wzj <wzj@ddbff460-bf18-274e-8a17-2e708027881f>
2021-09-17 04:24:11 +0000

    git-svn-id: https://39.108.144.9:8443/svn/afanti_shop@48 ddbff460-bf18-274e-8a17-2e708027881f

--- 1.0.0/app/Http/Resources/Home/CartResource/CartCollection.php
+++ 1.0.0/app/Http/Resources/Home/CartResource/CartCollection.php
@@ -18,6 +18,31 @@ class CartCollection extends ResourceCollection
     {
         return [
             'data'=>$this->collection->map(function($item){
+                $cart_list = $item->carts->map(function($cartItem){
+
+                    // 是否存在sku
+                    $goods_image = empty($cartItem->goods)?'':$this->thumb($cartItem->goods->goods_master_image,150);
+                    $goods_price = $cartItem->goods->goods_price??'0.00';
+                    $sku_name = '-';
+                    if(!empty($cartItem->goods_sku)){
+                        $goods_price = $cartItem->goods_sku->goods_price??'0.00';
+                        $sku_name = $cartItem->goods_sku->sku_name??'-';
+                    }
+                    return [
+                        'cart_id'               =>  $cartItem->id,
+                        'goods_id'              =>  $cartItem->goods_id,
+                        'sku_id'                =>  $cartItem->sku_id,
+                        'goods_name'            =>  $cartItem->goods->goods_name??__('goods.goods_not_found'),
+                        'buy_num'               =>  $cartItem->buy_num,
+                        'goods_image'           =>  $goods_image,
+                        'goods_price'           =>  $goods_price,
+                        'sku_name'              =>  $sku_name,
+                        'checked'               =>  false,
+                        'goods_stock' => $cartItem->goods->goods_stock??0,
+                        'goods_status' => !empty($cartItem->goods) && $cartItem->goods->goods_status == 1 ? 1 : 0,
+                    ];
+                });
+                $cart_list =  collect($cart_list->toArray());
                 return [
                     'store_id'              =>  $item->store->id,
                     'user_id'               =>  $item->user_id,
@@ -25,30 +50,7 @@ class CartCollection extends ResourceCollection
                     'store_logo'            =>  $item->store->store_logo??'',
                     'checked'               =>  false,
                     'css'                   =>  false,
-                    'cart_list'             =>  $item->carts->map(function($cartItem){
-
-                                                    // 是否存在sku
-                                                    $goods_image = empty($cartItem->goods)?'':$this->thumb($cartItem->goods->goods_master_image,150);
-                                                    $goods_price = $cartItem->goods->goods_price??'0.00';
-                                                    $sku_name = '-';
-                                                    if(!empty($cartItem->goods_sku)){
-                                                        $goods_price = $cartItem->goods_sku->goods_price??'0.00';
-                                                        $sku_name = $cartItem->goods_sku->sku_name??'-';
-                                                    }
-                                                    return [
-                                                        'cart_id'               =>  $cartItem->id,
-                                                        'goods_id'              =>  $cartItem->goods_id,
-                                                        'sku_id'                =>  $cartItem->sku_id,
-                                                        'goods_name'            =>  $cartItem->goods->goods_name??__('goods.goods_not_found'),
-                                                        'buy_num'               =>  $cartItem->buy_num,
-                                                        'goods_image'           =>  $goods_image,
-                                                        'goods_price'           =>  $goods_price,
-                                                        'sku_name'              =>  $sku_name,
-                                                        'checked'               =>  false,
-                                                        'goods_stock' => $cartItem->goods->goods_stock??0,
-                                                        'goods_status' => !empty($cartItem->goods) && $cartItem->goods->goods_status == 1 ? 1 : 0,
-                                                    ];
-                                                }),
+                    'cart_list'             =>  $cart_list->sortByDesc('goods_status')->values(),
                 ];
             }),
 
